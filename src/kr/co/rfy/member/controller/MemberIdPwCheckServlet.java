15165e6b7b5b399bc02fa76b5382d0519a769f88
kbl321 <adieu321@naver.com>
2021-12-21 12:33:54 +0900

    memberJoin.jsp : 이용약관(개인정보이용방침) 추가, AjaxIdCheckServlet.java, MemberIdPwdCheckServlet.java,MemberJoinServlet.java : if문 코드수정, 주석 추가, MemberJoinDataCheck.java : 회원가입정보 유효성 검사 로직 추가

--- src/kr/co/rfy/member/controller/MemberIdPwCheckServlet.java
+++ src/kr/co/rfy/member/controller/MemberIdPwCheckServlet.java
@@ -30,6 +30,7 @@ public class MemberIdPwCheckServlet extends HttpServlet {
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
 		request.setCharacterEncoding("UTF-8");
 		String inputUserId = request.getParameter("userId");
 		String inputUserPwd = request.getParameter("userPwd");
